959e0bf4e9d6beea7103aff6830631eb2651fb6e
Alberto Romano <aromanog1@gmail.com>
2020-09-05 00:10:28 -0500

    Shows a graph with the sales of PC and PE. Only works with those tho. All good for now.

--- Stock de InventarioVersion2.py
+++ Stock de InventarioVersion2.py
@@ -6,9 +6,9 @@ import time
 
 
 Excel_File = 'InventarioVersion2.xlsx'
-Lista = pd.read_excel(Excel_File,'Inventario')
-Nomenclatura = pd.read_excel(Excel_File,'Nomenclatura')
-Duracion = pd.read_excel(Excel_File,'Duracion')
+Lista = pd.read_excel(Excel_File,'Inventario')  #Inventory Data Frame
+Nomenclatura = pd.read_excel(Excel_File,'Nomenclatura')  #Nomenclatura Data Frame
+Duracion = pd.read_excel(Excel_File,'Duracion')     #Expiration Dates Data Frame
 
 nRows = len(Lista.axes[0])
 nCols = 0
@@ -22,6 +22,42 @@ while(Indice<nRows):
 for Columna in Lista.iloc[1]:
     nCols +=1
 
+
+
+#Sales Analysis Feature
+
+#First Feature: Graph analysis of your sales
+#Lista_Nomenclatura es una lista con todas las Nomenclaturas en el File
+#Lista.iloc[0] ---> First Row of the Lista Dataframe
+#Nombre, Inicial, Domingo1,D2,D3,D4
+
+def Sales_Graphs(Lista_Ventas, Producto):
+    #Lista de Ventas = Y's
+    #Domingos = X's
+    Valores_X = [1,2,3,4]
+
+    plt.scatter(Valores_X,Lista_Ventas)
+    plt.title('Ventas de ' + str(Producto))
+    plt.ylabel('Cantidad')
+    plt.xlabel('Semanas')
+    plt.show()
+    
+
+def Sales_Analysis(Lista_De_Productos):
+    for index in range(0,nRows):
+        Item_Actual = Lista.iloc[index]
+        if (Item_Actual[0] in Lista_De_Productos ):
+            Ventas = []
+            Cantidad_Anterior = Item_Actual[1]
+            for Columna in Item_Actual[2:6]:
+                Venta = abs(Columna-Cantidad_Anterior)
+                Ventas.append(Venta)
+                Cantidad_Anterior = Columna
+            Sales_Graphs(Ventas,Item_Actual[0])
+
+
+
+
 def Menu():
     print("-"*50)
     print("Bienvenido al sistema de reportes de alimentos.")
@@ -31,7 +67,8 @@ def Menu():
     print("2. Análisis de diversos productos")
     print("3. Análisis de todos los productos.")
     print("4. Ver toda la nomenclatura de los productos.")
-    print("5. Salir")
+    print("5. Probar Sales Analysis")
+    print("6. Salir")
     print("-"*50)
     while (True):
         try:
@@ -180,7 +217,7 @@ def Regresion_Lineal (index):
     plt.show()
 
 Opcion = Menu()
-while (Opcion != 5):
+while (Opcion != 6):
     Lista_De_Productos = []
     if (Opcion == 1):
         Producto = input("Ingresa el producto a analizar:")
@@ -217,34 +254,14 @@ while (Opcion != 5):
         Analisis(Lista_De_Productos)
     elif (Opcion == 4):
         Imprimir_Nomenclatura()
+    elif (Opcion == 5):
+        Sales_Analysis(['PC','PE'])
             
     
     Opcion = Continuar()
     if (Opcion == 1):
         Opcion = Menu()
     elif (Opcion == 2):
-        Opcion = 5
+        Opcion = 6
         print ("Gracias por usar los servicios de Bets!")
         print ("Hasta luego!")
-        
-
-
-      
-    elif (Opcion == 2):
-        Producto = input("Ingresa uno de los productos a analizar o ingrese NA para finalizar la lista:")
-        while (Producto != "NA"):
-            Estado = Comprobar_Nomenclatura(Producto)
-            if (Estado == 0): #Aka, el nombre es válido.
-                Lista_De_Productos.append(Producto)
-                print ("Alimentos a analizar:",Lista_De_Productos)
-            elif (Estado == 1): #Aka, el nombre no es válido
-                print ("Producto No Válido. Revise la tabla de Nomenclaturas para más información.")
-                print("")
-                time.sleep(1)
-            Producto = input("Ingresa uno de los productos a analizar o ingrese NA para finalizar la lista:")
-        if(len(Lista_De_Productos) > 0):
-            Analisis(Lista_De_Productos)
-        else:
-            print("No se puede realizar un análisis con una lista vacía.")
-
-        
